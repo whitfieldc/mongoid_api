115df95c1a525289d5eeec95f5654fc93d659753
Charles <whitfield.cw@gmail.com>
2015-11-23 07:25:07 -0800

    add rankings.csv and test opening seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'rankings.csv'))
+
+puts csv_text
