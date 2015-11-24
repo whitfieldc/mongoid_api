f18f45228a3751ec4b93cd8b7ca322e34239ca5d
Charles <whitfield.cw@gmail.com>
2015-11-23 22:22:32 -0800

    add support for query params

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,20 +1,24 @@
 class AppsController < ApplicationController
 
   def index
-    puts params
-
-    case params["type"]
-    when "paid"
-      app_type = "paid"
-    when "free"
-      app_type = "free"
-    end
 
     sort_type = (params["sort"] == "revenue") ? :revenue : :downloads
     limit = params["limit"] ? params["limit"].to_i : 400
 
+    case params["type"]
+    when "paid"
+      rejection_lambda = lambda {|app| app.price == 0}
+    when "free"
+      rejection_lambda = lambda {|app| app.price != 0}
+    else
+      rejection_lambda = lambda {|app| false }
+    end
 
-    @apps = Ranking.find(params[:ranking_id]).apps.order_by(avg_rating: 'desc').limit(4)
+    @apps = Ranking.find(params[:ranking_id])
+      .apps
+      .reject(&rejection_lambda)
+      .sort{|app_one, app_two| app_two[sort_type] <=> app_one[sort_type]}
+      .take(limit)
     render json: @apps
   end
 end
